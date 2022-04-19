7dc7f090e12f4c4037243d7c345be27adc018904
张三疯 <659966631@qq.com>
2022-04-19 20:42:55 +0800

    update

--- app/src/main/java/com/zxy/dialogdemo/activity/DialogActivity.kt
+++ app/src/main/java/com/zxy/dialogdemo/activity/DialogActivity.kt
@@ -64,14 +64,18 @@ class DialogActivity : AppCompatActivity() {
                 .setTransparency(0.2f)//可选                                  默认0.2f
                 .setCancelable(true) //可选                                   默认true
                 .setFullScreen(true)
-                .setEditFocus(R.id.tvDialgContent)
                 .setAnimator(AnimatorEnum.TRAN_T.VALUE)//可选，               默认AnimatorEnum.ZOOM.VALUE
                 .setOnClick(R.id.tvDialogConfirm) //可选  Dialog中的点击事件
+                .setOnDismissListeners(object :DialogInterface.OnDismissListener{
+                    override fun onDismiss(p0: DialogInterface?) {
+                        Log.e("zxy", "onDismiss")
+                    }
+
+                })
                 .OnClickListener { view, alertDialogUtils -> //必选                    点击事件的回调
                     when (view.id) {
-                        R.id.tvDialogConfig -> {
+                        R.id.tvDialogConfirm -> {
                             alertDialogUtils.dismiss()
-
                         }
                     }
                 }.show()
